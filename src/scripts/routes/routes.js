70f3a89f93d2a8835c70d14b4832da5920e0b0a2
filzaharry <harryfilza@gmail.com>
2021-08-01 18:34:50 +0700

    first commit

---
+++ src/scripts/routes/routes.js
@@ -0,0 +1,15 @@
+import Detail from "../views/pages/detail";
+import Like from "../views/pages/like";
+import NowPlaying from "../views/pages/now-playing";
+import Upcoming from "../views/pages/upcoming";
+
+ 
+const routes = {
+  '/': NowPlaying, // default page
+  '/now-playing': NowPlaying,
+  '/upcoming': Upcoming,
+  '/detail/:id': Detail,
+  '/like': Like,
+};
+ 
+export default routes;
